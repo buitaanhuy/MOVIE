e111a4ed14db585b558e67a6f35b346b5ea20169
Bui Tan Huy <buitaanhuy@gmail.com>
2024-01-04 11:14:47 +0700

    done

--- movie_infor.php
+++ movie_infor.php
@@ -1,4 +1,7 @@
+
+
 <?php
+        include 'main.html';
         if(isset($_GET['id']))
         {
             $id = $_GET['id'];
@@ -6,13 +9,15 @@
         $sql = "SELECT * FROM MOVIE WHERE id = $id";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
-            echo "<div class='list_movie'>";
+            echo "<div class='list_movie' style='display: block;'>";
         while($row = mysqli_fetch_assoc($result)) {
     ?>
     <div class="movie">
-        <img class="movie_img" src="image/<?php echo $row['image'];?>" alt="hình ảnh" style = "width: 200px;"> <br>
+        <img class="movie_img" src="image/<?php echo $row['image'];?>" alt="hình ảnh" style = "width: 300px;"> <br>
         <b><?php echo $row['movie_name']; ?></b> <br>
-        <?php echo $row['release_date']; ?> <br>
+        Release time: <?php echo $row['release_date']; ?> <br>
+        Popular: <?php echo $row['popularity'] ;?> <br>
+        Vote average: <?php echo $row['vote_average'];?> <br>
         <?php echo $row['overview'];?>
     </div>  
     <?php
--- popular.php
+++ popular.php
@@ -1,7 +1,9 @@
 <?php
         include 'main.html';
         require_once "config.php";
-        $sql = "SELECT * FROM MOVIE LIMIT 0,10";
+        $sql = "SELECT * FROM movie
+                WHERE popularity > 181 
+                ORDER BY popularity DESC";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) > 0) {
             echo "<div class='list_movie'>";
@@ -11,7 +13,8 @@
         <a class="movie_link" href="movie_infor.php?id=<?php echo $row['id'];?>" target = "blank">
             <img class="movie_img" src="image/<?php echo $row['image'];?>" alt="hình ảnh"> <br>
             <b><?php echo $row['movie_name']; ?></b> <br>
-            <?php echo $row['release_date']; ?>
+            Release time: <?php echo $row['release_date']; ?> <br>
+            Popular: <?php echo $row['popularity'] ;?>
         </a>
     </div>  
     <?php
